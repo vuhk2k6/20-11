d8f2ce4f07618dad46d665cce0f6fc7729ba74ec
vuhk2k6 <109848918+vuhk2k6@users.noreply.github.com>
2022-10-09 22:33:25 +0700

    up

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let items = document.querySelectorAll('.item');
-let nav = document.getElementById('nav');
-document.addEventListener('scroll', (event) => {
-    if(window.scrollY > 500){
-        nav.classList.add('tofixed');
-    }else{
-        nav.classList.remove('tofixed');
+let items = document.querySelectorAll(".item");
+let nav = document.getElementById("nav");
+document.addEventListener("scroll", (event) => {
+  if (window.scrollY > 500) {
+    nav.classList.add("tofixed");
+  } else {
+    nav.classList.remove("tofixed");
+  }
+  items.forEach((item) => {
+    if (item.offsetTop - window.scrollY < 350) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
     }
-    items.forEach(item =>{
-        if(item.offsetTop - window.scrollY < 350){
-            item.classList.add('active');
-        }
-    })
-})
+  });
+});
